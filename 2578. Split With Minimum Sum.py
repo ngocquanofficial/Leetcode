580c44d5a4c93598ab73fd96a6b54256e9ed28ce
Ngoc Quan <ngocquanofficial@gmail.com>
2023-03-18 00:23:02 +0700

    Beat 68.20% in Runtime, 2578. Split With Minimum Sum

---
+++ 2578. Split With Minimum Sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def splitNum(self, num):
+        digits = [int(i) for i in list(str(num))]
+        digits = sorted(digits)
+        maximum = 0
+        exp = 0
+        while digits :
+            first = digits.pop()
+            second = 0
+            if digits :
+                second = digits.pop()
+            maximum += (first + second) * 10**exp
+            exp += 1
+            
+        return maximum
+    
+solution = Solution()
+num = 1234
+print(solution.splitNum(num=num))
